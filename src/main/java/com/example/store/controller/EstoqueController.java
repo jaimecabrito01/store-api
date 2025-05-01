b5ef32d6874da093614bacf94fb2ecc05adeb3ba
jaimecabrito01 <azevedocarneirojaimevictor@gmail.com>
2025-05-01 12:16:18 -0300

    endpoints de estoqueController

--- src/main/java/com/example/store/controller/EstoqueController.java
+++ src/main/java/com/example/store/controller/EstoqueController.java
@@ -1,8 +1,28 @@
 package com.example.store.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.service.annotation.GetExchange;
+
+import com.example.store.model.Estoque;
+import com.example.store.service.EstoqueService;
 
 @Controller
+@RequestMapping("/estoque")
 public class EstoqueController {
+    @Autowired
+    private EstoqueService estoqueService;
+
     
+
+    @GetMapping("/all")
+    public List<Estoque> getAll(){
+        return estoqueService.listProdutos();
+    }
+
 }
